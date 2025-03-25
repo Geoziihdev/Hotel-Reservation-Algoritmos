d186561486078a961bf399f184ade8706350fd29
Geovana Santos <140776038+Geoziihdev@users.noreply.github.com>
2025-03-25 17:20:43 -0300

    Primeiro commit - Adicionando o projeto de hotel

---
+++ AuditorioAdequado/src/auditorioadequado/CadastroHospedes.java
@@ -0,0 +1,43 @@
+package auditorioadequado;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CadastroHospedes {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            ArrayList<String> hospedes = new ArrayList<>();
+            
+            OUTER:
+            while (true) {
+                System.out.println("Digite 1- cadastrar; 2- pesquisar; 3- sair");
+                int opcao = scanner.nextInt();
+                scanner.nextLine();
+                switch (opcao) {
+                    case 1 -> {
+                        if (hospedes.size() >= 15) {
+                            System.out.println("Máximo de cadastros atingido.");
+                        } else {
+                            System.out.print("Digite o nome do hóspede: ");
+                            hospedes.add(scanner.nextLine());
+                        }
+                    }
+                    case 2 -> {
+                        System.out.print("Digite o nome para pesquisa: ");
+                        String nome = scanner.nextLine();
+                        int index = hospedes.indexOf(nome);
+                        if (index != -1) {
+                            System.out.println("Hóspede " + nome + " foi encontrado no índice " + index);
+                        } else {
+                            System.out.println("Hóspede não encontrado.");
+                        }
+                    }
+                    case 3 -> {
+                        break OUTER;
+                    }
+                    default -> System.out.println("Opção inválida.");
+                }
+            }
+        }
+    }
+}
---
+++ AuditorioAdequado/src/auditorioadequado/OcupacaoHotel.java
@@ -0,0 +1,33 @@
+package auditorioadequado;
+
+import java.util.Scanner;
+
+
+public class OcupacaoHotel {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            boolean[][] hotel = new boolean[4][3];
+            
+            while (true) {
+                System.out.print("Informe Andar (1-4) e Quarto (1-3), ou 0 para sair: ");
+                int andar = scanner.nextInt();
+                if (andar == 0) break;
+                int quarto = scanner.nextInt();
+                
+                hotel[andar - 1][quarto - 1] = true;
+                
+                System.out.print("Deseja informar outra ocupação? (S/N): ");
+                if (scanner.next().equalsIgnoreCase("N")) break;
+            }
+
+            System.out.println("\nOcupação do hotel:");
+            for (int i = 0; i < 4; i++) {
+                System.out.print((i + 1) + "º andar: ");
+                for (int j = 0; j < 3; j++) {
+                    System.out.print("Quarto " + (j + 1) + (hotel[i][j] ? " ocupado | " : " desocupado | "));
+                }
+                System.out.println();
+            }
+        }
+    }
+}
